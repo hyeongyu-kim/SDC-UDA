454db2f54c4581059d32e96f8c2c3fbf04ecb35e
Hyeongyu Kim <77005104+hyeongyu-kim@users.noreply.github.com>
2023-08-25 03:08:34 +0900

    Update args.py

--- args.py
+++ args.py
@@ -17,39 +17,39 @@ def get_args():
 
     parser.add_argument("--task", type=str , default = 'cmd')    
     parser.add_argument("--dataset_used", type=str, default= 'CrossMODA' )
-    parser.add_argument("--datasets", type=int, default= ['to','tt'] ) #to , tt ,toce, flair
-    parser.add_argument("--N_seg_cls", type=int, default= 3 ) #to , tt ,toce, flair
+    parser.add_argument("--datasets", type=int, default= ['to','tt'] ) #T1 : to , T2 : tt 
+    parser.add_argument("--N_seg_cls", type=int, default= 3 ) # VS, C, bg
     
     # parser.add_argument("--task", type=str , default = 'fudan')    
     # parser.add_argument("--dataset_used", type=str, default= 'FUDAN' )
-    # parser.add_argument("--datasets", type=int, default= ['mr_fudan','ct_fudan'] ) #to , tt ,toce, flair
-    # parser.add_argument("--N_seg_cls", type=int, default= 5 ) #to , tt ,toce, flair
+    # parser.add_argument("--datasets", type=int, default= ['mr_fudan','ct_fudan'] ) #
+    # parser.add_argument("--N_seg_cls", type=int, default= 5 ) #
     
     
     # parser.add_argument("--task", type=str , default = 'synapse')    
     # parser.add_argument("--dataset_used", type=str, default= 'SYNAPSE' )
-    # parser.add_argument("--datasets", type=int, default= ['sy_t1','sy_t2'] ) #to , tt ,toce, flair  ### 'sy_ct'
-    # parser.add_argument("--N_seg_cls", type=int, default= 5 ) #to , tt ,toce, flair
+    # parser.add_argument("--datasets", type=int, default= ['sy_t1','sy_t2'] ) #
+    # parser.add_argument("--N_seg_cls", type=int, default= 5 ) #
     
 
 
-    parser.add_argument("--local_rank",         type=int, default=5)
-    parser.add_argument("--ex", type=str, default= 'Your Experiment Name') ## Update with mean , test with each
+    parser.add_argument("--local_rank",         type=int, default=0)   # GPU ID
+    parser.add_argument("--ex", type=str, default= 'Your_Experiment_Name') ## Every experiment
 
     parser.add_argument("--in_ch",                type=int,      default=3)
     
-    parser.add_argument("--save_freq",           type=int,    default=10000) #1000
-    parser.add_argument("--tensor_freq",         type=int,    default=500) #1000
-    parser.add_argument("--eval_freq",           type=int,    default=10000) #5000
+    parser.add_argument("--save_freq",           type=int,    default=10000) 
+    parser.add_argument("--tensor_freq",         type=int,    default=500) 
+    parser.add_argument("--eval_freq",           type=int,    default=10000) 
 
-    parser.add_argument("--lr", type=float,       default=5e-5) #CLF 0.0002
+    parser.add_argument("--lr", type=float,       default=5e-5) 
      
     parser.add_argument('--logfile',              type=str)
     
     parser.add_argument("--N_workers",             type=int,      default = 8)
     parser.add_argument('--manualSeed',            type=int,      default = 5688)        
-    parser.add_argument("--iter",                  type=int,      default = 10000000)
-    parser.add_argument("--batch",                 type=int,      default = 1) 
+    parser.add_argument("--iter",                  type=int,      default = 100000)
+    parser.add_argument("--batch",                 type=int,      default = 1)  ## Recommend batch 1, or need to modify 'EfficientSelfAttention' somewhere. 
     parser.add_argument("--r1",                    type=float,    default = 10)
     parser.add_argument("--d_reg_every",           type=int,      default=12)
     parser.add_argument("--start_iter",            type=int,      default=0)
@@ -58,7 +58,6 @@ def get_args():
     parser.add_argument("--ckpt",                  type=str, default=None)
     
     parser.add_argument("--channel", type=int, default=32)
-    #### ORIGINALLY 8 / 2048
     
     parser.add_argument("--channel_multiplier", type=int, default=1)
     parser.add_argument("--wandb", action="store_true")
@@ -73,7 +72,6 @@ def get_args():
     
     
     args.size= 256
-    args.channel = 16
     # args.batch = 1
 
     return args
